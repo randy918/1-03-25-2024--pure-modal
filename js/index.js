fbb0c0c58f3b076f6147e327724f3fc5bdaa6da1
Randy Miller <randy918@gmail.com>
2025-01-04 06:42:22 -0600

    perfect modal achieved 1-04-25-0642

---
+++ js/index.js
@@ -0,0 +1,89 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Your code here
+
+    document.body.style.backgroundColor = 'green';
+
+    //>  ████████████████████████████████████  CHILDREN
+
+
+
+
+
+
+    //>  ███████████████ OPEN WEATHER API d1a7fcdb2d6f38c6b216b37322444de9
+
+    //>  ████████████████████████████████████  BRIGHT BACKGROUND
+
+    function getBrightRandomRGB() {
+        const rrr = r(86, 255);
+        const ggg = r(86, 255);
+        const bbb = r(86, 255);
+        const rgbColor = `rgb(${rrr}, ${ggg}, ${bbb})`;
+        return rgbColor;
+    }
+
+    document.body.style.backgroundColor = getBrightRandomRGB();
+
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+
+    //_ ████████████████████████████████████  DATA STRUCTURES 
+
+
+    //>  ████████████████████████████████████  QUERY SELECTORS
+
+    const modal = document.querySelector('.modal');
+    const openModal = document.querySelector('.button__open');
+    const closeModal = document.querySelector('.button__close');
+
+    //>  ████████████████████████████████████  EVENT LISTENERS
+
+    openModal.addEventListener('click', () => {
+modal.showModal();
+    })
+
+    closeModal.addEventListener('click', () => {
+modal.close();
+    })
+
+
+
+
+
+
+
+
+
+
+
+    //>  ████████████████████████████████████  GENERAL PROGRAM
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+// console.log(document.documentElement);
+
+//< ████████████████████████████████████  MISC
+
+
+
